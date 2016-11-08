4ac42706d176df89c92fb632929c3aa4d0ea190d
Legys <legys5@yandex.ru>
2016-11-08 22:37:45 +0200

    Add files via upload

--- DB/mobiles.sql
+++ DB/mobiles.sql
@@ -1,5 +1,7 @@
 CREATE TABLE mobs (id INTEGER PRIMARY KEY, name TEXT, description TEXT, cost INTEGER);
-CREATE TABLE orders (order_composition TEXT, order_date DATE, order_count INTEGER);
+CREATE TABLE orders (id INTEGER PRIMARY KEY, order_date DATE);
+
+CREATE TABLE mobs_orders(order_id INT NOT NULL, mob_id INT NOT NULL, FOREIGN KEY (order_id) REFERENCES orders(id), FOREIGN KEY (mob_id) REFERENCES mobs(id));
 
 INSERT INTO mobs (name, description, cost)
 VALUES
@@ -16,25 +18,92 @@ VALUES
   ('Samsung A508', 'Display: 5 CPU: 2.4, RAM: 4 GB, Storage: 256 GB', 140),
   ('Samsung B1', 'Display: 5 CPU: 2.4, RAM: 4 GB, Storage: 256 GB', 200);
 
-INSERT INTO orders
+INSERT INTO orders (order_date)
 VALUES
-  ('Samsung 7 Note, Meizu MX6, Xiaomi MI 5', '2016-09-11', 1410),
-  ('Meizu Pro 5, Meizu MX6, Samsung A10', '2016-09-15', 1020),
-  ('Samsung 7 Note, Meizu MX6, Samsung A8', '2016-09-23', 1220),
-  ('Samsung J2, Meizu Pro 6, Meizu MX6', '2016-10-25', 930),
-  ('Samsung S7, Samsung 7 Note, Meizu MX6', '2016-10-29', 1980),
-  ('Samsung A10', '2016-11-01', 340),
-  ('Samsung A10, Samsung S6, Xiaomi MI 5', '2016-11-01', 1570),
-  ('Meizu MX6, Samsung J2, Samsung A8', '2016-11-02', 720),
-  ('Samsung J2', '2016-11-02', 200),
-  ('Samsung S7', '2016-11-03', 1000),
-  ('Samsung A8', '2016-11-03', 340),
-  ('Samsung 7 Note', '2016-11-04', 700),
-  ('Samsung S7, Samsung A8', '2016-11-05', 1240),
-  ('Samsung B1, Samsung A508', '2016-11-05', 340);
+  ('2016-09-11'),
+  ('2016-09-15'),
+  ('2016-09-23'),
+  ('2016-10-25'),
+  ('2016-10-29'),
+  ('2016-11-01'),
+  ('2016-11-01'),
+  ('2016-11-02'),
+  ('2016-11-02'),
+  ('2016-11-03'),
+  ('2016-11-03'),
+  ('2016-11-04'),
+  ('2016-11-05'),
+  ('2016-11-05'),
+  ('2016-11-06'),
+  ('2016-11-06'),
+  ('2016-11-07'),
+  ('2016-11-08'),
+  ('2016-11-08'),
+  ('2016-11-08');
 
-SELECT * FROM orders
-WHERE order_date BETWEEN '2016-10-29' AND '2016-11-05' AND order_count > 500;
-SELECT * FROM orders
-WHERE order_composition LIKE "%samsung%";
+INSERT INTO mobs_orders VALUES
+(1, 2),
+(1, 3),
+(1, 1),
+(2, 1),
+(2, 2),
+(3, 5),
+(4, 12),
+(4, 6),
+(4, 6),
+(5, 9),
+(6, 2),
+(7, 5),
+(7, 8),
+(8, 1),
+(9, 6),
+(9, 8),
+(9, 8),
+(10, 10),
+(10, 1),
+(10, 9),
+(11, 7),
+(12, 3),
+(13, 2),
+(14, 5),
+(14, 11),
+(14, 9),
+(14, 6),
+(15, 3),
+(15, 7),
+(15, 4),
+(15, 5),
+(15, 12),
+(16, 10),
+(17, 4),
+(18, 8),
+(18, 11),
+(18, 6),
+(19, 7),
+(20, 9);
+
+
+SELECT
+  mobs_orders.order_id,
+  mobs_orders.mob_id AS 'Mob ID',
+  mobs.name AS 'Phone',
+  orders.order_date AS 'Order date',
+  count(mobs_orders.order_id) AS 'Amount of items',
+  SUM((SELECT mobs.cost FROM mobs WHERE mobs_orders.mob_id = mobs.id)) AS 'Total price'
+  FROM mobs, orders
+JOIN mobs_orders ON mob_id = mobs.id
+                  AND order_id = orders.id
+  WHERE orders.order_date BETWEEN DATE('NOW', '-7 day') AND DATE ('NOW')
+GROUP BY order_id
+HAVING SUM(mobs.cost) > 500;
+
+SELECT
+  mobs.name,
+  orders.order_date
+FROM  mobs, orders
+  JOIN mobs_orders ON mob_id = mobs.id
+                  AND order_id = orders.id
+WHERE mobs.name LIKE '%samsung%';
+
+
 
